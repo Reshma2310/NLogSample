6ca6a01caff04ac31c975466a2fcc4225b09e713
Reshma2310 <dreshma253@gmail.com>
2022-05-26 20:31:22 +0500

    [Reshma] - NLog Sample execution using Generics Program

---
+++ Generics/Generics/Generic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Generic
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
+        double[] percent = { 1.2, 2.3, 22.6, 50.7, 0.85 };
+        Char[] Alphabets = { 'R', 'E', 'S', 'D' };
+
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = NLog.LogManager.GetCurrentClassLogger();
+            log.Info("Generics Program Started");
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            double[] percent = { 1.2, 2.3, 22.6, 50.7, 0.85 };
+            Char[] Alphabets = { 'R', 'E', 'S', 'D' };
+
+            Program.toPrint<int>(numbers);
+            Program.toPrint<double>(percent);
+            Program.toPrint<char>(Alphabets);
+        }
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+    }
+}
